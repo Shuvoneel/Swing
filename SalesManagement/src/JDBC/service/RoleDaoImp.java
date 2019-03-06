78559e1d2e81e23905d8d868322df92b28abfa5e
Shuvoneel <mhshuvo29@gmail.com>
2019-03-06 18:46:51 +0600

    show database table data into swing window table

--- SalesManagement/src/JDBC/service/RoleDaoImp.java
+++ SalesManagement/src/JDBC/service/RoleDaoImp.java
@@ -4,13 +4,16 @@ import JDBC.connection.DBConnection;
 import JDBC.dao.RoleDao;
 import JDBC.pojo.Role;
 import JDBC.view.DatabaseTool;
-import java.sql.Connection;
+import com.mysql.jdbc.Util;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RoleDaoImp implements RoleDao {
-
-
 
     @Override
     public void createTable(String sql) {
@@ -27,7 +30,7 @@ public class RoleDaoImp implements RoleDao {
     public void save(Role role) {
         String sql = "insert into role(role_name) values(?)";
         try {
-            PreparedStatement pstm = DatabaseTool.conn.prepareCall(sql);
+            PreparedStatement pstm = DatabaseTool.conn.prepareStatement(sql);
             pstm.setString(1, role.getRoleName());
             pstm.execute();
             System.out.println("Data Inserted");
@@ -54,7 +57,19 @@ public class RoleDaoImp implements RoleDao {
 
     @Override
     public List<Role> getRoles() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Role> list = new ArrayList<>();
+        String sql = "select * from role";
+        try {
+            PreparedStatement ps = DBConnection.getDBConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Role role = new Role(rs.getInt(1), rs.getString(2));
+                list.add(role);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RoleDaoImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
     }
 
 }
--- SalesManagement/src/JDBC/view/DatabaseTool.java
+++ SalesManagement/src/JDBC/view/DatabaseTool.java
@@ -73,7 +73,7 @@ public class DatabaseTool extends javax.swing.JFrame {
         lblDisplay.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel2.setText("Host");
 
         txtHost.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -87,15 +87,15 @@ public class DatabaseTool extends javax.swing.JFrame {
         txtPort.setText("3306");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel4.setText("DB Name");
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel5.setText("Username");
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel6.setText("Password");
 
         txtDB.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -137,12 +137,11 @@ public class DatabaseTool extends javax.swing.JFrame {
                                 .addGap(25, 25, 25)
                                 .addComponent(jScrollPane1))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE))
-                                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+                                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 5, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(txtUser, javax.swing.GroupLayout.Alignment.TRAILING)
